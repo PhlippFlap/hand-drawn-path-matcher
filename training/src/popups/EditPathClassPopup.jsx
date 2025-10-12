404e0a2398e2319606b6fef2562b5d98bd0e4b7c
Philipp da Silva Fonseca <philipp-fonseca@outlook.de>
2025-10-12 17:48:57 +0200

    EditPathClassPopup

--- training/src/popups/EditPathClassPopup.jsx
+++ training/src/popups/EditPathClassPopup.jsx
@@ -9,14 +9,31 @@ function EditPathClassPopup() {
     return (
         <Popup>
             <div className='popup'>
-                <label>
-                    Symbol name:
-                    <input name="symbolNameInput" />
-                </label>
-                <label>
-                    Path class name:
-                    <input name="pathClassNameInput" />
-                </label>
+                <div className='fieldContainer'>
+                    <div className='field'>
+                        <label htmlFor="symbolName">
+                        Symbol Name:
+                        </label>
+                        <input id="symbolName" type="text" name="symbolNameInput" placeholder="my_fancy_rune"/>
+                    </div>
+                    <p>
+                        This is the name of the symbol the path class represents. 
+                        The name has to be unique.
+                    </p>
+                </div>
+                <div className='fieldContainer'>
+                    <div className='field'>
+                        <label htmlFor="pathName">
+                        Path Class Name:
+                        </label>
+                        <input id="pathName" type="text" name="pathClassNameInput" placeholder="my_fancy_rune_left_to_right"/>
+                    </div>
+                    <p>
+                        This is the name of the class path. 
+                        A class path is sensitive to the direction the symbol is drawn.
+                        There can be multiple class paths per symbol.
+                    </p>
+                </div>
                 <div className='bottomButtons'>
                     <RoundButton onClick={() => setPopup(null)} backgroundColor={'var(--gray)'}>
                         Cancel
