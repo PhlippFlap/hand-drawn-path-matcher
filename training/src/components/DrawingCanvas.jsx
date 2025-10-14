c93ca80ce1a47043eb2ea9194b7ef4aae6e3f284
Philipp da Silva Fonseca <philipp-fonseca@outlook.de>
2025-10-14 23:20:54 +0200

    Tried to pass props to sketch bat cant get it to work

--- training/src/components/DrawingCanvas.jsx
+++ training/src/components/DrawingCanvas.jsx
@@ -1,23 +1,27 @@
 import { ReactP5Wrapper } from "@p5-wrapper/react";
 
-function sketch(p5) {
-  p5.setup = () => p5.createCanvas(600, 400, p5.WEBGL);
+function sketch(p5, props) {
+    const { color = 200 } = props; // Default color if not provided
 
-  p5.draw = () => {
-    p5.background(250);
-    p5.normalMaterial();
-    p5.push();
-    p5.rotateZ(p5.frameCount * 0.01);
-    p5.rotateX(p5.frameCount * 0.01);
-    p5.rotateY(p5.frameCount * 0.01);
-    p5.plane(100);
-    p5.pop();
-  };
+    p5.setup = () => {
+        p5.createCanvas(100, 100, p5.WEBGL);
+    };
+
+    p5.draw = () => {
+        p5.background(color); // Use the passed color prop
+        p5.normalMaterial();
+        p5.push();
+        p5.rotateZ(p5.frameCount * 0.01);
+        p5.rotateX(p5.frameCount * 0.01);
+        p5.rotateY(p5.frameCount * 0.01);
+        p5.plane(100);
+        p5.pop();
+    };
 }
 
 function DrawingCanvas() {
     return (
-        <ReactP5Wrapper sketch={sketch} />
+        <ReactP5Wrapper sketch={sketch} color={100} />
     );
 }
 
