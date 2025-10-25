5e9c3e83e32dff7af7ec50ae450f9c4da04f18aa
Philipp da Silva Fonseca <philipp-fonseca@outlook.de>
2025-10-25 16:41:18 +0200

    Bugfix

--- training/python/algorithm.py
+++ training/python/algorithm.py
@@ -7,7 +7,10 @@ def train_all(negatives_seq_class, other_seq_classes: list[SequenceClass]):
         negative_classes = [negatives_seq_class] + other_seq_classes[:i] + other_seq_classes[i+1:]
         negative_sequences = sum(list(map(lambda c: c.prepared_sequences, negative_classes)), [])
         seq_class = other_seq_classes[i]
-        print(f"Training class with name {seq_class.className} and symbol-name {seq_class.symbolName} ...")
+        if (len(seq_class.prepared_sequences) < 2): # require at least 2 positives
+            print(f"Could not train class '{seq_class.className}' since there are not enough sequences!")
+            continue
+        print(f"Training class with name '{seq_class.className}' and symbol-name '{seq_class.symbolName}' ...")
         strong_learner = StrongLearner()
         strong_learner.train(seq_class.prepared_sequences, negative_sequences)
         strong_learner.print_info()
